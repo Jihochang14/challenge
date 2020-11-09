6880e249182230db7aa3fa4d45751129f5d874fe
cometonf <cometonf@naver.com>
2020-11-09 01:26:11 +0000

    update cos decay for lr

--- train_density.py
+++ train_density.py
@@ -1,6 +1,8 @@
 import argparse
 import numpy as np
 import os
+from pathlib import Path
+import json
 import pickle
 import tensorflow as tf
 from tensorflow.keras.callbacks import *
@@ -18,6 +20,7 @@ from train_frame import custom_scheduler
 from transforms import *
 from utils import *
 from models import transformer_layer
+from adamp_tf import AdamP
 
 np.set_printoptions(precision=4)
 
@@ -37,13 +40,14 @@ args.add_argument('--n_mels', type=int, default=128)
 
 # DATA
 args.add_argument('--background_sounds', type=str,
-                  default='generate_wavs/codes/drone_normed_complex.pickle')
+                  default='generate_wavs/codes/drone_normed_complex_v2.pickle')
 args.add_argument('--voices', type=str,
                   default='generate_wavs/codes/voice_normed_complex.pickle')
 args.add_argument('--labels', type=str,
                   default='generate_wavs/codes/voice_labels_mfc.npy')
 args.add_argument('--noises', type=str,
                   default='generate_wavs/codes/noises_specs.pickle')
+args.add_argument('--test', default=True, action='store_false')
 args.add_argument('--test_background_sounds', type=str,
                   default='generate_wavs/codes/test_drone_normed_complex.pickle')
 args.add_argument('--test_voices', type=str,
@@ -53,15 +57,18 @@ args.add_argument('--test_labels', type=str,
 
 # TRAINING
 args.add_argument('--optimizer', type=str, default='adam',
-                                 choices=['adam', 'sgd', 'rmsprop'])
+                                 choices=['adam', 'sgd', 'rmsprop', 'adamp'])
 args.add_argument('--lr', type=float, default=0.001)
 args.add_argument('--lr_factor', type=float, default=0.7)
 args.add_argument('--lr_patience', type=int, default=10)
+args.add_argument('--lr_scheduler', type=str, default='rsqrt',
+                                    choices=['rsqrt', 'cos'])
 
 args.add_argument('--epochs', type=int, default=500)
 args.add_argument('--batch_size', type=int, default=8)
 args.add_argument('--n_frame', type=int, default=2048)
 args.add_argument('--steps_per_epoch', type=int, default=100)
+args.add_argument('--val_steps_per_epoch', type=int, default=24)
 args.add_argument('--l2', type=float, default=1e-6)
 
 # AUGMENTATION
@@ -72,7 +79,6 @@ args.add_argument('--max_noises', type=int, default=5)
 
 args.add_argument('--reverse', type=bool, default=True)
 args.add_argument('--multiplier', type=float, default=10.)
-MULTIPLIER = 10
 
 
 def safe_div(x, y, eps=EPSILON):
@@ -124,13 +130,15 @@ def augment(specs, labels, time_axis=1, freq_axis=0):
     return specs, labels
 
 
-def preprocess_labels(x, y):
-    # process y: [None, time, classes] -> [None, time', classes]
-    for i in range(5):
-        # sum_pool1d
-        y = tf.nn.avg_pool1d(y, 2, strides=2, padding='SAME') * 2
-    y *= MULTIPLIER
-    return x, y
+def make_preprocess_labels(multiplier):
+    def preprocess_labels(x, y):
+        # process y: [None, time, classes] -> [None, time', classes]
+        for i in range(5):
+            # sum_pool1d
+            y = tf.nn.avg_pool1d(y, 2, strides=2, padding='SAME') * 2
+        y *= multiplier
+        return x, y
+    return preprocess_labels
 
 
 def to_density_labels(x, y):
@@ -172,66 +180,40 @@ def make_dataset(config, training=True):
     pipeline = pipeline.map(complex_to_magphase)
     pipeline = pipeline.map(magphase_to_mel(config.n_mels))
     pipeline = pipeline.map(minmax_log_on_mel)
-    pipeline = pipeline.map(preprocess_labels)
+    pipeline = pipeline.map(make_preprocess_labels(config.multiplier))
     return pipeline.prefetch(AUTOTUNE)
 
 
-def d_total(y_true, y_pred, apply_round=True):
-    y_true /= MULTIPLIER
-    y_pred /= MULTIPLIER
+def make_d_total(multiplier):
+    def d_total(y_true, y_pred, apply_round=True):
+        y_true /= multiplier
+        y_pred /= multiplier
 
-    # [None, time, 30] -> [None, time, 3, 10]
-    y_true = tf.stack(tf.split(y_true, 3, axis=-1), axis=-2)
-    y_pred = tf.stack(tf.split(y_pred, 3, axis=-1), axis=-2)
+        # [None, time, 30] -> [None, time, 3, 10]
+        y_true = tf.stack(tf.split(y_true, 3, axis=-1), axis=-2)
+        y_pred = tf.stack(tf.split(y_pred, 3, axis=-1), axis=-2)
 
-    # d_dir
-    d_true = tf.reduce_sum(y_true, axis=(-3, -2))
-    d_pred = tf.reduce_sum(y_pred, axis=(-3, -2))
-    if apply_round:
-        d_true = tf.math.round(d_true)
-        d_pred = tf.math.round(d_pred)
-    d_dir = D_direction(d_true, d_pred)
+        # d_dir
+        d_true = tf.reduce_sum(y_true, axis=(-3, -2))
+        d_pred = tf.reduce_sum(y_pred, axis=(-3, -2))
+        if apply_round:
+            d_true = tf.math.round(d_true)
+            d_pred = tf.math.round(d_pred)
+        d_dir = D_direction(d_true, d_pred)
 
-    # c_cls
-    c_true = tf.reduce_sum(y_true, axis=(-3, -2))
-    c_pred = tf.reduce_sum(y_pred, axis=(-3, -2))
-    if apply_round:
-        c_true = tf.math.round(c_true)
-        c_pred = tf.math.round(c_pred)
-    d_cls = D_class(c_true, c_pred)
+        # c_cls
+        c_true = tf.reduce_sum(y_true, axis=(-3, -2))
+        c_pred = tf.reduce_sum(y_pred, axis=(-3, -2))
+        if apply_round:
+            c_true = tf.math.round(c_true)
+            c_pred = tf.math.round(c_pred)
+        d_cls = D_class(c_true, c_pred)
 
-    return 0.8 * d_dir + 0.2 * d_cls
-
-
-def d_total_z(y_true, y_pred, apply_round=True):
-    y_true /= MULTIPLIER
-    y_pred = K.zeros_like(y_pred)
-    y_pred /= MULTIPLIER
-
-    # [None, time, 30] -> [None, time, 3, 10]
-    y_true = tf.stack(tf.split(y_true, 3, axis=-1), axis=-2)
-    y_pred = tf.stack(tf.split(y_pred, 3, axis=-1), axis=-2)
-
-    # d_dir
-    d_true = tf.reduce_sum(y_true, axis=(-3, -2))
-    d_pred = tf.reduce_sum(y_pred, axis=(-3, -2))
-    if apply_round:
-        d_true = tf.math.round(d_true)
-        d_pred = tf.math.round(d_pred)
-    d_dir = D_direction(d_true, d_pred)
-
-    # c_cls
-    c_true = tf.reduce_sum(y_true, axis=(-3, -2))
-    c_pred = tf.reduce_sum(y_pred, axis=(-3, -2))
-    if apply_round:
-        c_true = tf.math.round(c_true)
-        c_pred = tf.math.round(c_pred)
-    d_cls = D_class(c_true, c_pred)
-
-    return 0.8 * d_dir + 0.2 * d_cls
+        return 0.8 * d_dir + 0.2 * d_cls
+    return d_total
 
 
-def custom_loss(y_true, y_pred, alpha=0.8, l2=0.5):
+def custom_loss(y_true, y_pred, alpha=0.8, l2=1.0):
     # y_true, y_pred = [None, time, 30]
     # [None, time, 30] -> [None, time, 3, 10]
     t_true = tf.stack(tf.split(y_true, 3, axis=-1), axis=-2)
@@ -287,13 +269,31 @@ def cos_sim(y_true, y_pred):
         axis=-1)
 
 
+def warmup_cosine_decay(lr=0.1, epochs=500, warmup=10):
+    def func(epoch):
+        if epoch <= warmup:
+            return lr * ((epoch + 1) / warmup) ** 2
+        else:
+            return lr * 0.5 * (1 + np.cos(np.pi * (epoch - warmup) / np.float32(epochs - warmup)))
+    return func
+
+
 if __name__ == "__main__":
     config = args.parse_args()
     print(config)
 
     TOTAL_EPOCH = config.epochs
     BATCH_SIZE = config.batch_size
-    NAME = (config.name if config.name.endswith('.h5') else config.name + '.h5')
+
+
+    """ LOG """
+    NAME = Path('logs', config.name)
+    CSV_NAME = NAME / (config.name + '.csv')
+    H5_NAME = NAME / (config.name + '.h5')
+    os.system(f'mkdir -p {str(NAME)}')
+    os.system(f'touch {str(CSV_NAME)}')
+    json.dump(config.__dict__, open(str(NAME / (config.name + '.json')), 'w'), indent=2, sort_keys=False)
+
 
     """ MODEL """
     x = tf.keras.layers.Input(shape=(config.n_mels, config.n_frame, 2))
@@ -336,7 +336,9 @@ if __name__ == "__main__":
         print('loaded pretrained model')
 
     if config.optimizer == 'adam':
-        opt = Adam(config.lr, clipvalue=0.01) 
+        opt = Adam(config.lr, clipvalue=0.01)
+    elif config.optimizer == 'adamp':
+        opt = AdamP(config.lr, clipvalue=0.01)
     elif config.optimizer == 'sgd':
         opt = SGD(config.lr, momentum=0.9)
     else:
@@ -350,22 +352,28 @@ if __name__ == "__main__":
         model = multi_gpu_model(model, gpus=len(config.gpus))
     model.compile(optimizer=opt, 
                   loss=custom_loss,
-                  metrics=[d_total, d_total_z, cos_sim])
+                  metrics=[make_d_total(config.multiplier), cos_sim])
     #model.summary()
     
 
     """ DATA """
     train_set = make_dataset(config, training=True)
-    test_set = make_dataset(config, training=False)
+    test_set = make_dataset(config, training=False) if config.test else None
+
 
     """ TRAINING """
+    if config.lr_scheduler == 'rsqrt':
+        lr_scheduler = custom_scheduler(4096, TOTAL_EPOCH/12)
+    elif config.lr_scheduler == 'cos':
+        lr_scheduler = warmup_cosine_decay(lr=config.lr, epochs=TOTAL_EPOCH, warmup=10)
+
     callbacks = [
-        CSVLogger(NAME.replace('.h5', '.log'),
+        CSVLogger(str(CSV_NAME), #NAME.replace('.h5', '.log'),
                   append=True),
-        LearningRateScheduler(custom_scheduler(4096, TOTAL_EPOCH/12)),
+        LearningRateScheduler(lr_scheduler),
         SWA(start_epoch=TOTAL_EPOCH//2, swa_freq=2),
-        ModelCheckpoint(NAME,
-                        monitor='val_d_total', 
+        ModelCheckpoint(H5_NAME,#NAME
+                        monitor='val_d_total' if config.test else 'd_total', 
                         save_best_only=True,
                         verbose=1),
         TerminateOnNaN()
@@ -376,7 +384,7 @@ if __name__ == "__main__":
               batch_size=BATCH_SIZE,
               steps_per_epoch=config.steps_per_epoch,
               validation_data=test_set,
-              validation_steps=12,
+              validation_steps=config.val_steps_per_epoch,
               callbacks=callbacks)
 
-    model.save(NAME.replace('.h5', '_SWA.h5'))
+    # model.save(NAME.replace('.h5', '_SWA.h5'))
